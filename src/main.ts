24a589845fc308b3d06a4bef74e7cd4c87fce7c7
Lim Kian Yee <89409516+k1any33@users.noreply.github.com>
2022-09-08 19:46:36 +0800

    feat: add swagger, docker and github workflow for deployment to heroku (#2)
    * feat: add swagger
    
    * fix: remove express dependencies
    
    * feat: add docker file and docker compose for dev
    
    * feat: add github workflow file

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,25 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Grant Disbursement API')
+    .setDescription(
+      'A RESTful API that would help your team decide on the groups of people who are eligible for various upcoming government grants',
+    )
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(8080, '0.0.0.0');
 }
 bootstrap();
