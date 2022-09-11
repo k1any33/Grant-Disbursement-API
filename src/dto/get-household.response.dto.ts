fa7fedbfda49a314983b03024deba8a5ffb91caf
Lim Kian Yee <89409516+k1any33@users.noreply.github.com>
2022-09-11 16:06:54 +0800

    fix: modify dto response output on swagger (#20)

--- src/dto/get-household.response.dto.ts
+++ src/dto/get-household.response.dto.ts
@@ -5,6 +5,6 @@ export class GetHouseholdsResponseDto {
   @ApiProperty({ type: 'array', example: [] })
   items: HouseholdResponseDto[]
 
-  @ApiProperty({ type: 'number', example: 1 })
+  @ApiProperty({ type: 'number', example: 0 })
   count: number
 }
